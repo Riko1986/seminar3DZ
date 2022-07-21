fae00ef6cd309bf8a961875d7a15d74759d79ca4
Riko1986 <107243671+Riko1986@users.noreply.github.com>
2022-07-21 09:03:30 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+int n = Convert.ToInt32(Console.ReadLine());
+int n1, n2, n4, n5;
+n1 = n / 10000;
+n2 = (n % 10000) / 1000;
+n4 = (n % 100) / 10;
+n5 = n % 10;
+if(n1 == n5 && n2 == n4){
+
+Console.WriteLine("Это число палиндром");
+}
+else{
+    Console.WriteLine("Нет");
+}
+
